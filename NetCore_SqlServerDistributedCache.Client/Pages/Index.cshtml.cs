cb1657899cd5b38720cac2fcea0b46c73657dffb
Marlon <marlonfsolisest@gmail.com>
2026-01-12 07:11:32 -0500

    Move Session tests to its own page.

--- NetCore_SqlServerDistributedCache.Client/Pages/Index.cshtml.cs
+++ NetCore_SqlServerDistributedCache.Client/Pages/Index.cshtml.cs
@@ -5,29 +5,21 @@ namespace NetCore_SqlServerDistributedCache.Client.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
         private readonly ICacheService _cache;
         private readonly ISessionService _session;
 
         public IndexModel(
-            ILogger<IndexModel> logger, 
-            ICacheService cache,
-            ISessionService session)
+            ICacheService cache)
         {
-            _logger = logger;
             _cache = cache;
-            _session = session;
         }
 
         /* Properties section ********************************************************************************/
 
         public List<KeyValuePair<string, string>> CacheValueList { get; set; } = [];
-        public List<KeyValuePair<string, string>> SessionValueList { get; set; } = [];
 
         [BindProperty] public string CacheKeyName { get; set; } = string.Empty;
         [BindProperty] public string CacheKeyValue { get; set; } = string.Empty;
-        [BindProperty] public string SessionKeyName { get; set; } = string.Empty;
-        [BindProperty] public string SessionKeyValue { get; set; } = string.Empty;
 
 
 
@@ -45,17 +37,7 @@ namespace NetCore_SqlServerDistributedCache.Client.Pages
             }
         }
 
-        private async Task SetSessionValueList()
-        {
-            IEnumerable<string> keys = await _session.GetKeysAsync();
-            foreach (string key in keys)
-            {
-                string? sessionValue = await _session.GetAsync<string>(key);
-                if (sessionValue is null) continue;
-                KeyValuePair<string, string> kvp = new(key, sessionValue);
-                SessionValueList.Add(kvp);
-            }
-        }
+
 
 
         /* Public Methods section ********************************************************************************/
@@ -66,7 +48,6 @@ namespace NetCore_SqlServerDistributedCache.Client.Pages
 
             // Set up the list of KeyValue Cache/Session values
             await SetCacheValueList();
-            await SetSessionValueList();
         }
 
         public async Task OnPostSetCacheItem()
@@ -86,24 +67,6 @@ namespace NetCore_SqlServerDistributedCache.Client.Pages
             await _cache.ClearAsync();
             await SetCacheValueList();
         }
-
-        
-        public async Task OnPostSetSessionItem()
-        {   
-            await _session.SetAsync(CacheKeyName, CacheKeyValue);
-            await SetSessionValueList();
-        }
-
-        public async Task OnPostRemoveSessionItem(string key)
-        {
-            await _session.RemoveAsync(key);
-            await SetSessionValueList();
-        }
-
-        public async Task OnPostClearSession()
-        {
-            await _session.ClearAsync();
-            await SetSessionValueList();
-        }        
+     
     }
 }
---
+++ NetCore_SqlServerDistributedCache.Client/Pages/SessionServiceTests.cshtml.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NetCore_SqlServerDistributedCache.Client.Pages;
+
+public class SessionServiceTestsModel : PageModel
+{
+    private readonly ISessionService _session;
+
+    public SessionServiceTestsModel(ISessionService session)
+    {
+        _session = session;
+    }
+
+    /* Properties section ********************************************************************************/
+
+    public List<KeyValuePair<string, string>> SessionValueList { get; set; } = [];
+
+    [BindProperty] public string SessionKeyName { get; set; } = string.Empty;
+    [BindProperty] public string SessionKeyValue { get; set; } = string.Empty;
+
+
+    /* Private Methods section ********************************************************************************/
+
+    private async Task SetSessionValueList()
+    {
+        IEnumerable<string> keys = await _session.GetKeysAsync();
+        foreach (string key in keys)
+        {
+            string? sessionValue = await _session.GetAsync<string>(key);
+            if (sessionValue is null) continue;
+            KeyValuePair<string, string> kvp = new(key, sessionValue);
+            SessionValueList.Add(kvp);
+        }
+    }
+
+
+    /* Public Methods section ********************************************************************************/
+
+    public async Task OnGet()
+    {
+        // Set up the list of KeyValue Cache/Session values
+        await SetSessionValueList();
+    }
+
+    public async Task OnPostSetSessionItem()
+    {
+        await _session.SetAsync(SessionKeyName, SessionKeyValue);
+        await SetSessionValueList();
+    }
+
+    public async Task OnPostRemoveSessionItem(string key)
+    {
+        await _session.RemoveAsync(key);
+        await SetSessionValueList();
+    }
+
+    public async Task OnPostClearSession()
+    {
+        await _session.ClearAsync();
+        await SetSessionValueList();
+    }
+}
